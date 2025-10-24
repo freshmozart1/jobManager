1e39b79563a1509357f2811e535813ca0998a45c
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2025-10-24 22:09:38 +0200

    Add job search Fixes #3

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,20 +1,70 @@
+'use client';
 import { AppJobsTable } from "@/components/ui/appJobsTable";
-import { NoBaseUrlError } from "@/lib/errors";
-import { runFilterAgentMock } from "@/lib/filterAgentMock";
-import { ObjectId } from "mongodb";
-import { Suspense, useCallback, useMemo } from "react";
+import AppPromptsGrid from "@/components/ui/appPromptsGrid";
+import { useEffect, useState } from "react";
+import useToUrl from "@/hooks/useToUrl";
+import { LoaderCircle } from "lucide-react";
 
 export default function SearchPage() {
-    const BASE_URL = useMemo(() => process.env.BASE_URL, []);
-    if (!BASE_URL) throw new NoBaseUrlError();
-    const fetchJobs = useCallback((apiEndpoint: string) => fetch(apiEndpoint).then(res => res.json()).then(data => (data as Job[]).map(job => ({ ...job, postedAt: new Date(job.postedAt) }))), []);
+    const toUrl = useToUrl();
+    const [dbJobs, setDbJobs] = useState<JobWithNewFlag[] | null>(null);
+    const [prompts, setPrompts] = useState<PromptDocument[] | null>(null);
+    const [promptId, setPromptId] = useState<string | null>(null);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        Promise.all([
+            fetch(toUrl('/api/jobs'), { signal: controller.signal }).then(res => res.json()).then((data: Job[]) => data.map(job => ({ ...job, postedAt: new Date(job.postedAt), new: false }))),
+            fetch(toUrl('/api/prompts?agentType=filter'), { signal: controller.signal }).then(res => res.json()).then((data: PromptDocument[]) => data.map(prompt => ({ ...prompt, createdAt: new Date(prompt.createdAt), updatedAt: new Date(prompt.updatedAt) })))
+        ]).then(([jobs, prompts]) => {
+            setDbJobs(jobs);
+            setPrompts(prompts);
+        });
+        return () => controller.abort();
+    }, [toUrl]);
+
+    useEffect(() => {
+        console.log('promptId changed:', promptId);
+        const controller = new AbortController();
+        if (promptId) {
+            console.log('Starting filtering for promptId:', promptId);
+            fetch(toUrl('/api/mock/filter'), {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                signal: controller.signal,
+                body: JSON.stringify({
+                    promptId,
+                    actorName: "curious_coder/linkedin-jobs-scraper"
+                })
+            }).then(res => res.json()).then((data: FilterAgentResult) => {
+                data.jobs = data.jobs.map(job => ({ ...job, postedAt: new Date(job.postedAt), new: true }));
+                data.rejects = data.rejects.map(job => ({ ...job, postedAt: new Date(job.postedAt) }));
+                setDbJobs(prevDbJobs => {
+                    if (!prevDbJobs) return data.jobs.map(j => ({ ...j, new: true }));
+                    const existingJobIds = new Set(prevDbJobs.map(j => j.id));
+                    const newJobs = data.jobs.filter(j => !existingJobIds.has(j.id)).map(j => ({ ...j, new: true }));
+                    return [...newJobs, ...prevDbJobs];
+                });
+            }).catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('Filtering fetch aborted');
+                } else {
+                    console.error('Error during filtering fetch:', err);
+                }
+            }).finally(() => setPromptId(null));
+        }
+        return () => controller.abort();
+    }, [promptId, toUrl]);
+
     return (
-        <Suspense fallback={<div>Loading jobs...</div>}>
-            <AppJobsTable
-                jobsPromise={fetchJobs(`${BASE_URL}/api/jobs`)}
-                filterAgent={runFilterAgentMock(new ObjectId(), { artificialDelayMsPerJob: 1000 })}
-                className="col-span-4 col-start-1"
-            />
-        </Suspense>
+        dbJobs && prompts
+            ? <>
+                <AppJobsTable jobs={dbJobs} />
+                <AppPromptsGrid prompts={prompts} onClick={(prompt: PromptDocument) => setPromptId(prompt._id)} filterAgentRunning={promptId !== null} />
+            </>
+            : <div className="w-full h-svh flex flex-col gap-4 items-center justify-center">
+                <p>Loading jobs and prompts from database</p>
+                <LoaderCircle className="animate-spin" />
+            </div>
     );
 }
--- components/ui/appPromptsGrid.tsx
+++ components/ui/appPromptsGrid.tsx
@@ -1,33 +1,27 @@
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Play, LoaderCircle, SquarePen } from "lucide-react";
 
 type AppPromptsGridProps = {
     prompts: PromptDocument[];
-    onClick: (promptId: string) => void;
+    onClick: (prompt: PromptDocument) => void;
+    filterAgentRunning: boolean;
     className?: string;
 };
-export default function AppPromptsGrid({ prompts, className, onClick }: AppPromptsGridProps) {
-    return <div className={`${className} grid grid-cols-3 auto-rows-auto gap-4`}>
+export default function AppPromptsGrid({ prompts, className, onClick, filterAgentRunning }: AppPromptsGridProps) {
+    return <div className={`${className ?? ''} grid grid-cols-3 auto-rows-auto gap-4`}>
         {prompts.length
-            ? prompts.map((prompt, i) => <Card key={i}>
+            ? prompts.map((prompt, i) => <Card key={i} className={(i + 1) % 3 === 1 ? 'rounded-l-none' : (i + 1) % 3 === 0 ? 'rounded-r-none' : ''}>
                 <CardHeader>
                     <CardTitle>{prompt.name}</CardTitle>
-                    <CardDescription>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>Created at:</td>
-                                    <td>{prompt.createdAt.toLocaleString()}</td>
-                                </tr>
-                                <tr>
-                                    <td>Updated at:</td>
-                                    <td>{prompt.updatedAt.toLocaleString()}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </CardDescription>
                 </CardHeader>
-                <CardFooter>
-                    <button onClick={() => onClick(prompt._id)}>Select</button>
+                <CardFooter className="inline-flex justify-between">
+                    <Button className="cursor-pointer" onClick={() => onClick(prompt)} disabled={filterAgentRunning}>
+                        {filterAgentRunning ? <LoaderCircle className="animate-spin" /> : <Play />}
+                    </Button>
+                    <Button className="cursor-pointer" variant="ghost">
+                        <SquarePen />
+                    </Button>
                 </CardFooter>
             </Card>)
             : null}

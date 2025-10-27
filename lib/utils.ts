2faa82970d794ced6c8c6e8c941bdc254c17cd07
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2025-10-27 11:30:13 +0100

    Added function chunkArray

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,13 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 export async function sleep(ms: number) { return new Promise(r => setTimeout(r, ms)); }
+
+
+export function chunkArray<T>(array: T[], size: number): T[][] {
+  if (size <= 0) throw new Error("Chunk size must be greater than 0");
+  const result: T[][] = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+}

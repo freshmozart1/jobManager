79cb7819c611f77df4b5fd9acd7ffc70f229a782
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2025-10-27 11:28:07 +0100

    Added MongoDB plyground for deleting all jobs

---
+++ app/api/filter/deleteAll.mongodb.js
@@ -0,0 +1,9 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+// eslint-disable-next-line
+use('applicationAgentDB');
+
+// Create a new document in the collection.
+db.getCollection('jobs').deleteMany({});

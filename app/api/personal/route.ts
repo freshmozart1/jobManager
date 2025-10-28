3fa3b0925be8cb55b98c98ba18210908b8f884da
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2025-10-28 11:37:50 +0100

    Added GET route for personal information.

---
+++ app/api/personal/route.ts
@@ -0,0 +1,17 @@
+import { corsHeaders } from "@/lib/cors";
+import { NoDatabaseNameError } from "@/lib/errors";
+import mongoPromise from "@/lib/mongodb";
+import { fetchPersonalInformation } from "@/lib/personal";
+import { NextResponse } from "next/server";
+
+export function OPTIONS() {
+    return new NextResponse(null, { headers: corsHeaders() });
+}
+
+export async function GET() {
+    const DATABASE_NAME = process.env.DATABASE_NAME;
+    if (!DATABASE_NAME) return NextResponse.json({}, { status: 500, statusText: NoDatabaseNameError.name });
+    const db = (await mongoPromise).db(DATABASE_NAME);
+    await db.command({ ping: 1 }, { timeoutMS: 3000 });
+    return NextResponse.json(await fetchPersonalInformation(db), { headers: corsHeaders() });
+}
